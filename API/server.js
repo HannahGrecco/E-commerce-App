b2448db6adb46ab4634d5d9bb2ebdf79e7dc5013
Hannah <grecco327@gmail.com>
2025-07-14 20:52:07 -0300

    removed .env to add to .gitignore

--- API/server.js
+++ API/server.js
@@ -1,11 +1,15 @@
 import express from "express"
 import dotenv from 'dotenv'
 import connectDB from './src/database/db.js'
+import authRoutes from './src/routes/authRoutes.js'
 dotenv.config();
 
 const app = express()
 connectDB()
 
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
 
 
 app.listen (3000)
